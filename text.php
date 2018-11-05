88b2428d0bcc67ba7fa8f0ea77a6b75b51e54dd4
WangYan <2567069002@qq.com>
2018-11-05 09:29:48 +0800

    11.5

---
+++ text.php
@@ -0,0 +1,73 @@
+<?php
+
+//1.
+// 一
+// $sum=0;
+// for($i=1;$i<=100;$i++)
+// {
+//     $sum+=$i;
+// }
+// echo $sum;
+
+//二
+// $n=100;
+// echo (1+$n)*($n/2);
+
+//三
+// echo array_sum(range(1,100));
+
+//2.
+// function factorial($n){
+
+//     $j=1;
+
+//     for($i=1;$i<=$n;$i++){
+
+//         $j*=$i;
+
+//     }
+//     return $j;
+// }
+
+//3.
+// function ishuiwen($str){
+//   $len=strlen($str);
+//   $l=1;
+//   $k=intval($len/2)+1;
+//    for($j=0;$j<$k;$j++){
+//     if (substr($str,$j,1)!=substr($str,$len-$j-1,1))
+//      {
+//   $l=0;
+//   break;
+//    }
+//  }
+//  if ($l==1)
+//  {
+//  return 1;
+//  }
+//  else
+//  {
+//  return -1;
+//   }
+//  }
+//  $str=12321;
+//  echo ishuiwen($str);
+
+
+
+
+//4.
+// class Person{
+// 	var $name;
+// 	var $age;
+// }
+// class Student EXTENDS Person{
+// 	$rend1 = new Student;
+// 	$rpmd = new Student;
+// 	$data = new Student;
+// 	$tpm = new Student;
+// 	$gind = new Student;
+// 	$resn = new Student;
+// 	$dbsa = new Student;
+// 	$tmrd = new Student;
+// }
